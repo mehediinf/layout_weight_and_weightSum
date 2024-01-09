1785400419737303dbdf63d205e5e5dd9f525816
mehedi-hasan <mehediinform@gmail.com>
2024-01-10 03:12:43 +0600

    1st Commit layout_weight_and_weightSum

---
+++ app/src/main/java/com/example/layout_weight_and_weightsum/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.layout_weight_and_weightsum;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "layout_weight_and_weightSum"
+include(":app")
+ 
